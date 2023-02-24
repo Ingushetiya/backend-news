b1baf5fce922c254938c1ff56fe2ce902a9fc406
Said <saidmartazanov1@mail.ru>
2023-02-24 23:26:58 +0300

    jwt

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express"
-// import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken"
 import mongoose from "mongoose"
 import { validationResult } from "express-validator"
 import bcrypt from "bcrypt"
@@ -16,22 +16,45 @@ mongoose
 
 app.post("/auth/register", registerValidator, async (req, res) => {
   // const { email, fullName, password, avatarUrl } = req.body
-  const errors = validationResult(req)
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array())
-  }
-  const password = req.body.password
-  const salt = await bcrypt.genSalt(10)
-  const passwordHash = await bcrypt.hash(password, salt)
-  const doc = new UserModel({
-    email: req.body.email,
-    fullName: req.body.fullName,
-    avatarUrl: req.body.avatarUrl,
-    password: passwordHash
-  })
+  try {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array())
+    }
+    const password = req.body.password
+    const salt = await bcrypt.genSalt(10)
+    const passwordHash = await bcrypt.hash(password, salt)
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash,
+    })
 
-  const user = await doc.save()
-  res.json(user)
+    const user = await doc.save()
+
+    const token = jwt.sign({
+      _id: user._id,
+    },
+      "secret123",
+      {
+        expiresIn: "30d"
+      },
+    )
+
+    res.json({
+      ...user,
+      token,
+    })
+
+  } catch (error) {
+
+    console.log(error)
+
+    res.status(500).json({
+      message: "Не удалось зарегестироваться"
+    })
+  }
 })
 
 app.listen(PORT, (err) => {
@@ -41,10 +64,7 @@ app.listen(PORT, (err) => {
   console.log("Server start");
 })
 
-// const token = jwt.sign({
-//   email: req.body.email,
-//   fullname: "Вася Пупкин"
-// }, "secret123")
+
 
 // res.json({
 //   succes: true,
