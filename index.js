f808c8df2833186c853804befab8e932819d33f0
Said <saidmartazanov1@mail.ru>
2023-02-24 23:00:18 +0300

    Authorization

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 import express from "express"
-import jwt from "jsonwebtoken"
+// import jwt from "jsonwebtoken"
 import mongoose from "mongoose"
+import { validationResult } from "express-validator"
+import bcrypt from "bcrypt"
+import UserModel from "./models/User.js"
+import { registerValidator } from "./validations/auth.js"
 
 const app = express()
 const PORT = 4000
 app.use(express.json())
 mongoose
-  .connect("mongodb+srv://ingushetiya:89286953059aA@cluster0.tgpnukm.mongodb.net/test")
+  .connect("mongodb+srv://ingushetiya:89286953059aA@cluster0.tgpnukm.mongodb.net/Blog-News")
   .then(() => console.log("Connect DB"))
   .catch((err) => console.log("DB error", err))
 
-app.post("/auth/login", (req, res) => {
-  const token = jwt.sign({
+app.post("/auth/register", registerValidator, async (req, res) => {
+  // const { email, fullName, password, avatarUrl } = req.body
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array())
+  }
+  const password = req.body.password
+  const salt = await bcrypt.genSalt(10)
+  const passwordHash = await bcrypt.hash(password, salt)
+  const doc = new UserModel({
     email: req.body.email,
-    fullname: "Вася Пупкин"
-  }, "secret123")
-
-  res.json({
-    succes: true,
-    token
+    fullName: req.body.fullName,
+    avatarUrl: req.body.avatarUrl,
+    password: passwordHash
   })
+
+  const user = await doc.save()
+  res.json(user)
 })
 
 app.listen(PORT, (err) => {
@@ -28,3 +40,13 @@ app.listen(PORT, (err) => {
   }
   console.log("Server start");
 })
+
+// const token = jwt.sign({
+//   email: req.body.email,
+//   fullname: "Вася Пупкин"
+// }, "secret123")
+
+// res.json({
+//   succes: true,
+//   token
+// })
