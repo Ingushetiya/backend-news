2532fc15254eaa2a2c84bcc0875c63db6a2e6d5b
Said <saidmartazanov1@mail.ru>
2023-02-26 14:42:23 +0300

    Закончил создание функционала для статьей CRUD

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 import express from "express"
-import mongoose, { get } from "mongoose"
+import mongoose from "mongoose"
 
 import checkAuth from './utils/checkAuth.js'
-import { registerValidator } from "./validations/validations.js"
 
-import { register, getMe, login } from "./controller/UserController.js"
+import { registerValidator, loginValidator, postCreateValidator } from "./validations/validations.js"
+
+import * as UserController from "./controller/UserController.js"
+import * as PostController from "./controller//PostController.js"
+
 
 const app = express()
 const PORT = 4000
 app.use(express.json())
+
 mongoose
   .connect("mongodb+srv://ingushetiya:89286953059aA@cluster0.tgpnukm.mongodb.net/Blog-News")
   .then(() => console.log("Connect DB"))
   .catch((err) => console.log("DB error", err))
 
-app.post("/auth/login", login)
-app.post("/auth/register", registerValidator, register)
 
-app.get("/auth/me", checkAuth, getMe)
+
+
+app.post("/auth/login", loginValidator, UserController.login)
+app.post("/auth/register", registerValidator, UserController.register)
+app.get("/auth/me", checkAuth, UserController.getMe)
+
+
+app.get('/posts', PostController.getAll)
+app.get("/posts/:id", PostController.getOne)
+app.post("/posts", checkAuth, postCreateValidator, PostController.createPost)
+app.delete("/posts/:id", checkAuth, PostController.remove)
+app.patch("/posts/:id", checkAuth, PostController.update)
+
 
 app.listen(PORT, (err) => {
   if (err) {
